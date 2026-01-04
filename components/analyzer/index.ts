af4998140d46f1a3b94fbc359511418bf7740a8d
ahmedaa0378-dot <ahmed.aa0378@gmail.com>
2026-01-03 23:09:09 -0500

    Updated index.ts

---
+++ components/analyzer/index.ts
@@ -0,0 +1,3 @@
+export { default as HumanBodySVG } from './HumanBodySVG';
+export { default as SymptomMindMap } from './SymptomMindMap';
+export { default as AIChatInterface } from './AIChatInterface';
